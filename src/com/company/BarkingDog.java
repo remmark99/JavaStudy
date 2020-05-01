bf1d5ae3d8b7b74c196654f847e3b7c4c3679bf4
Виктор Доска <vdoska@direct-credit.ru>
2020-05-01 15:53:06 +0500

    Excercise 3: Barking Dog

---
+++ src/com/company/BarkingDog.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class BarkingDog {
+    public static void main(String[] args) {
+    }
+
+    public static boolean shouldWakeUp(boolean isBarking, int hourOfDay) {
+        return hourOfDay >= 0 && hourOfDay <= 23 && isBarking && (hourOfDay < 8 || hourOfDay > 22);
+    }
+}
