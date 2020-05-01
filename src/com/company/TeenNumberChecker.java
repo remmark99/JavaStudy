56196c44f65437be06fae304ec9e488bd8e37025
Виктор Доска <vdoska@direct-credit.ru>
2020-05-01 16:36:45 +0500

    Excercise 7: Teen Number Checker

---
+++ src/com/company/TeenNumberChecker.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class TeenNumberChecker {
+    public static void main(String[] args) {
+    }
+
+    public static boolean hasTeen(int firstAge, int secondAge, int thirdAge) {
+        return isTeen(firstAge) || isTeen(secondAge) || isTeen(thirdAge);
+    }
+
+    public static boolean isTeen(int age) {
+        return age > 12 && age < 20;
+    }
+}
