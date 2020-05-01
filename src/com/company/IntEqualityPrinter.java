9a10b080ba40ba1281aaa2fab606188cc9d0144d
Виктор Доска <vdoska@direct-credit.ru>
2020-05-01 16:55:47 +0500

    Excercise 10: Equality Printer

---
+++ src/com/company/IntEqualityPrinter.java
@@ -0,0 +1,20 @@
+package com.company;
+
+public class IntEqualityPrinter {
+    public static void main(String[] args) {
+    }
+
+    public static void printEqual(int firstNumber, int secondNumber, int thirdNumber) {
+        if (firstNumber >= 0 && secondNumber >= 0 && thirdNumber >= 0) {
+            if (firstNumber == secondNumber && secondNumber == thirdNumber) {
+                System.out.println("All numbers are equal");
+            } else if (firstNumber != secondNumber && secondNumber != thirdNumber && firstNumber != thirdNumber) {
+                System.out.println("All numbers are different");
+            } else {
+                System.out.println("Neither all are equal or different");
+            }
+        } else {
+            System.out.println("Invalid Value");
+        }
+    }
+}
