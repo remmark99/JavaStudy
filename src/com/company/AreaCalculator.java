1c083867b99fe7e360b2d722f2c762ebe4b5ecaf
Виктор Доска <vdoska@direct-credit.ru>
2020-05-01 16:40:37 +0500

    Excercise 8: Area Calculator

---
+++ src/com/company/AreaCalculator.java
@@ -0,0 +1,18 @@
+package com.company;
+
+public class AreaCalculator {
+    public static void main(String[] args) {
+    }
+
+    public static double area(double radius) {
+        if (radius >= 0) return Math.PI * radius * radius;
+
+        return -1d;
+    }
+
+    public static double area(double width, double length) {
+        if (width >= 0 && length >= 0) return width * length;
+
+        return -1d;
+    }
+}
