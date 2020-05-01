82657a7f3ebda3a1b91b5af3d2a538a97e4658c7
Виктор Доска <vdoska@direct-credit.ru>
2020-05-01 16:47:00 +0500

    Excercise 9: Minutes To Years And Days Calculator

---
+++ src/com/company/MinutesToYearsDaysCalculator.java
@@ -0,0 +1,17 @@
+package com.company;
+
+public class MinutesToYearsDaysCalculator {
+    public static void main(String[] args) {
+    }
+
+    public static void printYearsAndDays(long minutes) {
+        if (minutes < 0) {
+            System.out.println("Invalid Value");
+        } else {
+            int years = (int) minutes / (60 * 24 * 365);
+            int days = (int) minutes % (60 * 24 * 365) / (60 * 24);
+
+            System.out.println(minutes + " min = " + years + " y and " + days + " d");
+        }
+    }
+}
