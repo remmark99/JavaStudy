13a870199514c751e06ec7d3e0fb56d13effbb94
Виктор Доска <vdoska@direct-credit.ru>
2020-05-01 15:37:02 +0500

    Excercise 1: Speed Converter

---
+++ src/com/company/SpeedConverter.java
@@ -0,0 +1,26 @@
+package com.company;
+
+public class SpeedConverter {
+
+
+    public static void main(String[] args) {
+        printConversion(-1);
+    }
+
+    public static long toMilesPerHour(double kilometersPerHour) {
+        if (kilometersPerHour < 0) return -1L;
+
+        return Math.round(kilometersPerHour / 1.609d);
+    }
+
+    public static void printConversion(double kilometersPerHour) {
+        long milesPerHour = toMilesPerHour(kilometersPerHour);
+
+        if (milesPerHour == -1) {
+            System.out.println("Invalid value!");
+            return;
+        }
+
+        System.out.println(kilometersPerHour + " km/h = " + milesPerHour + " mi/h");
+    }
+}
