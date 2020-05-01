29209d98988991795c4232e877b343d9463f1ce6
Виктор Доска <vdoska@direct-credit.ru>
2020-05-01 16:32:55 +0500

    Excercise 6: Equal Sum Checker

---
+++ src/com/company/EqualSumChecker.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class EqualSumChecker {
+    public static void main(String[] args) {
+    }
+
+    public static boolean hasEqualSum(int firstNumber, int secondNumber, int result) {
+        return firstNumber + secondNumber == result;
+    }
+}
