421f8add9214ebe16f51c44e26af57ae95c406bc
Виктор Доска <vdoska@direct-credit.ru>
2020-05-03 23:18:34 +0500

    Excercise 31: Wall Area

---
+++ src/thirdHomework/Wall.java
@@ -0,0 +1,35 @@
+package thirdHomework;
+
+public class Wall {
+    private double width;
+    private double height;
+
+    public Wall() {
+        this(0, 0);
+    }
+
+    public Wall(double width, double height) {
+        this.width = width > 0 ? width : 0;
+        this.height = height > 0 ? height : 0;
+    }
+
+    public double getWidth() {
+        return width;
+    }
+
+    public void setWidth(double width) {
+        this.width = width > 0 ? width: 0;
+    }
+
+    public double getHeight() {
+        return height;
+    }
+
+    public void setHeight(double height) {
+        this.height = height > 0 ? height : 0;
+    }
+
+    public double getArea() {
+        return this.width * this.height;
+    }
+}
